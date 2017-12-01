22945153c8becfe166a3767902add5fde1253640
Jim Selders <jimselders@hotmail.com>
2017-12-01 16:31:42 -0500

    Production TLS enforcement (via 302).

--- ToDoAPI/Startup.cs
+++ ToDoAPI/Startup.cs
@@ -16,15 +16,21 @@ namespace ToDoAPI
 	/// <summary>
 	/// Startup class
 	/// </summary>
+	/// <remarks>
+	/// public class StartupDevelopment
+	/// public class StartupProduction
+	/// </remarks>
 	public class Startup
 	{
 		/// <summary>
 		/// Startup class constructor, with injected config
 		/// </summary>
 		/// <param name="configuration"></param>
-		public Startup(IConfiguration configuration)
+		/// <param name="env"></param>
+		public Startup(IConfiguration configuration, IHostingEnvironment env)
 		{
 			Configuration = configuration;
+			Environment = env;
 		}
 
 		/// <summary>
@@ -33,12 +39,21 @@ namespace ToDoAPI
 		public IConfiguration Configuration { get; }
 
 		/// <summary>
+		/// 
+		/// </summary>
+		public IHostingEnvironment Environment { get; }
+
+		/// <summary>
 		/// This method gets called by the runtime. Use this method to add services to the container.
 		/// </summary>
 		/// <param name="services"></param>
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddDbContext<ToDoContext>(opt => opt.UseInMemoryDatabase("ToDoList")); // in-memory database
+			services.AddDbContext<ToDoContext>(opt =>
+			{
+				opt.UseInMemoryDatabase("ToDoList");
+				//opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+			});
 
 			// Respect request for XML or JSON responses from us
 			// https://blog.jeremylikness.com/5-rest-api-designs-in-dot-net-core-1-29a8527e999c
@@ -47,28 +62,29 @@ namespace ToDoAPI
 				options.RespectBrowserAcceptHeader = true;
 				options.FormatterMappings.SetMediaTypeMappingForFormat("xml", MediaTypeHeaderValue.Parse("text/xml"));
 				options.FormatterMappings.SetMediaTypeMappingForFormat("json", MediaTypeHeaderValue.Parse("application/json"));
-				options.Filters.Add(new Microsoft.AspNetCore.Mvc.RequireHttpsAttribute());
+				if (Environment?.IsProduction() ?? false)
+					options.Filters.Add(new Microsoft.AspNetCore.Mvc.RequireHttpsAttribute());  // TLS
 			})
 			.AddXmlSerializerFormatters();
 
 			// Register the Swagger generator, defining one or more Swagger documents
 			services.AddSwaggerGen(c =>
-				{
-					c.SwaggerDoc("v1", new Info
-					{
-						Version = "v1",
-						Title = "ToDo API",
-						Description = "A simple example ASP.NET Core Web API",
-						TermsOfService = "None",
-						Contact = new Contact { Name = "Jimmy Bobby", Email = "", Url = "https://twitter.com/jimselders11" },
-						License = new License { Name = "Unlicensed!", Url = "https://cbsnews.com" }
-					});
+						{
+							c.SwaggerDoc("v1", new Info
+							{
+								Version = "v1",
+								Title = "ToDo API",
+								Description = "A simple example ASP.NET Core Web API",
+								TermsOfService = "None",
+								Contact = new Contact { Name = "Jimmy Bobby", Email = "", Url = "https://twitter.com/jimselders11" },
+								License = new License { Name = "Unlicensed!", Url = "https://cbsnews.com" }
+							});
 
-					// Set the comments path for the Swagger JSON and UI.
-					var basePath = PlatformServices.Default.Application.ApplicationBasePath;
-					var xmlPath = Path.Combine(basePath, "ToDoApi.xml");
-					c.IncludeXmlComments(xmlPath);
-				});
+							// Set the comments path for the Swagger JSON and UI.
+							var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+							var xmlPath = Path.Combine(basePath, "ToDoApi.xml");
+							c.IncludeXmlComments(xmlPath);
+						});
 		}
 
 		/// <summary>
